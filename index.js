731e099e46f6b39e55a5e188701f3a4f9be95de4
Li Ge <gelber.libi@gmail.com>
2018-04-04 10:09:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return "Happy " + `${holiday}` + ", " + `${name}` + "!";
 }
+
+function holidayCountdown(holiday, days) {
+  return 'returns "It\'s ${days} days until ${holiday}!"';
+}
